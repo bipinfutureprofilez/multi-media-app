e819713e142be59dbe9d2c243b9a1be0d52793e0
Bipin <bipinkr7742@gmail.com>
2024-05-27 18:21:35 +0530

    working on app file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 // require("dotenv").config({path: "/.env"})
-
+import { app } from "./app.js";
 import connectDB from "./db/connection.js";
 
+const port = process.env.PORT || 8000;
+
 connectDB();
+(async () => {
+    try {
+        app.on("error", (error) => {
+            console.log("Error: ", error);
+            throw error;
+        })
+        app.listen(port, () => console.log(`Server is running at: ${port}`))
+
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+})()
